d6e479e7125257cde8cf9c5a5c0d2950bbbd74b7
lllyl2012 <229707363@qq.com>
2019-12-09 20:20:39 +0800

    first

---
+++ CrawlModule.py
@@ -0,0 +1,3 @@
+import json
+from pyquery import PyQuery as pq
+from MyUtil import get_page
---
+++ CustomError.py
@@ -0,0 +1,4 @@
+
+class PoolEmptyError(RuntimeError):
+    def __init__(self, arg):
+        self.args = arg
